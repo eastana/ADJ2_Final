eace617e2ebde84c59a880623b0989e47b83787b
danafrommoon <a.orunbassarova@astanait.edu.kz>
2021-03-04 23:02:09 +0500

    Edited Admin priviligies

---
+++ src/main/java/kz/edu/astanait/ajp2_final_project/controllers/AdminController.java
@@ -0,0 +1,49 @@
+package kz.edu.astanait.ajp2_final_project.controllers;
+
+import kz.edu.astanait.ajp2_final_project.models.User;
+import kz.edu.astanait.ajp2_final_project.services.UserService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class AdminController {
+    private UserService userService;
+
+    @GetMapping("/deleteUser/{id}")
+    public String deleteUser(@PathVariable(value = "id") long id) {
+
+        // call delete user method for ADMIN
+        this.userService.deleteUserById(id);
+        return "redirect:/admin";
+    }
+    @PostMapping("/saveUser")
+    public String saveUser(@ModelAttribute("user") User user) {
+        // save user to database
+        userService.register(user);
+        return "redirect:/admin";
+    }
+    @GetMapping("/showFormForUpdate/{id}")
+    public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
+
+        // get user from the service
+        User user = userService.getUserById(id);
+
+        // set user as a model attribute to pre-populate the form
+        model.addAttribute("user", user);
+        return "update_user";
+    }
+    @GetMapping("/admin")
+    public String viewList(Model model){
+        model.addAttribute("listUsers",userService.getAllUsers());
+        return "admin";
+    }
+
+    @GetMapping("/showNewUserForm")
+    public String showNewUserForm(Model model){
+        User user = new User();
+        model.addAttribute("user", user);
+        return "new_user";
+    }
+}
--- src/main/java/kz/edu/astanait/ajp2_final_project/controllers/UserController.java
+++ src/main/java/kz/edu/astanait/ajp2_final_project/controllers/UserController.java
@@ -31,44 +31,10 @@ public class UserController {
         userService.register(userForm);
         return "login";
     }
-    @GetMapping("/deleteUser/{id}")
-    public String deleteUser(@PathVariable(value = "id") long id) {
-
-        // call delete user method for ADMIN
-        this.userService.deleteUserById(id);
-        return "redirect:/admin";
-    }
-    @PostMapping("/saveUser")
-    public String saveUser(@ModelAttribute("user") User user) {
-        // save user to database
-        userService.register(user);
-        return "redirect:/admin";
-    }
-    @GetMapping("/showFormForUpdate/{id}")
-    public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
-
-        // get user from the service
-        User user = userService.getUserById(id);
-
-        // set user as a model attribute to pre-populate the form
-        model.addAttribute("user", user);
-        return "update_user";
-    }
-    @GetMapping("/admin")
-    public String viewList(Model model){
-        model.addAttribute("listUsers",userService.getAllUsers());
-        return "admin";
-    }
     @GetMapping("/profile")
     public String viewProfile(Model model){
         model.addAttribute("listUsers",userService.getAllUsers());
         return "profile";
-    }
-    @GetMapping("/showNewUserForm")
-    public String showNewUserForm(Model model){
-        User user = new User();
-        model.addAttribute("user", user);
-        return "new_user";
     }
 
 }
